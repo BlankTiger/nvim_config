aa1ceed90c0ece0ec8fb7dc05d33e83c63981aa7
Maciej Urban <maciej.urban@nokia.com>
2023-04-26 08:50:51 +0200

    vim-just

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -64,6 +64,6 @@ return {
 	{ "AckslD/nvim-neoclip.lua", dependencies = { "nvim-telescope/telescope.nvim" } },
 	{ dir = "/home/blanktiger/Projects/unorphanize.nvim" },
 	"vale1410/vim-minizinc",
-	"IndianBoy42/tree-sitter-just",
+	"NoahTheDuke/vim-just",
 	--[[ { "chrisgrieser/nvim-spider", lazy = true }, ]]
 }
