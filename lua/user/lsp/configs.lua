46adb32038e7f7978885504916437efb3bf3b6cb
Maciej Urban <maciej-urban@outlook.com>
2023-07-02 11:29:27 +0200

    added nix lsp

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -9,7 +9,7 @@ local mason_lspconfig = require("mason-lspconfig")
 
 local servers = { "pyright", "rust_analyzer", "ltex", "texlab", "jsonls", "lua_ls", "dockerls", "tsserver", "clangd",
   "cssls", "emmet_ls", "html", "marksman", "taplo", "yamlls", "jdtls", "marksman", "kotlin_language_server", "asm_lsp",
-  "bashls", "tailwindcss"
+  "bashls", "tailwindcss", "nil"
 }
 -- ruff_lsp
 -- local servers = { "pyright", "rust_analyzer", "powershell_es", "asm_lsp", "dockerls", "elixirls", "texlab", "vimls", "terraformls", "jsonls", "sumneko_lua", "tsserver", "ansiblels", "clangd", "cssls", "cssmodules_ls", "emmet_ls", "html", "jdtls", "julials", "marksman" }
