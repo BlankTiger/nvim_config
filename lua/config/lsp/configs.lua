2da5135ee19ad9f93fc5d61b752db45873e59d33
Maciej Urban <blanktiger@pop-os.localdomain>
2023-11-17 03:33:26 +0100

    feat: reduced amount of work in lspconfig

--- lua/config/lsp/configs.lua
+++ lua/config/lsp/configs.lua
@@ -8,7 +8,7 @@ local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
 
 local servers = {
-  "ocamllsp",
+  -- "ocamllsp",
   -- "ocamlformat",
   "gopls",
   -- "pyright",
@@ -20,19 +20,15 @@ local servers = {
   "texlab",
   "jsonls",
   "lua_ls",
-  "dockerls",
   "tsserver",
-  "clangd",
   "cssls",
   "emmet_ls",
   "html",
   "marksman",
   "taplo",
   "yamlls",
-  "jdtls",
   "marksman",
   "kotlin_language_server",
-  "asm_lsp",
   "bashls",
   "tailwindcss"
 }
@@ -47,11 +43,11 @@ for _, server in pairs(servers) do
     on_attach = require("config.lsp.handlers").on_attach,
     capabilities = require("config.lsp.handlers").capabilities,
   }
-
-  local has_custom_opts, server_custom_opts = pcall(require, "config.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
-  end
+  --
+  -- local has_custom_opts, server_custom_opts = pcall(require, "config.lsp.settings." .. server)
+  -- if has_custom_opts then
+  --   opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+  -- end
 
   if server == "ltex" then
     require("lspconfig").ltex.setup {
@@ -75,19 +71,19 @@ for _, server in pairs(servers) do
     goto continue
   end
 
-  if server == "pyright" then
-    require("lspconfig").pyright.setup {
-      on_attach = opts.on_attach,
-      settings = {
-        python = {
-          analysis = {
-            typeCheckingMode = "off",
-          },
-        },
-      }
-    }
-    goto continue
-  end
+  -- if server == "pyright" then
+  --   require("lspconfig").pyright.setup {
+  --     on_attach = opts.on_attach,
+  --     settings = {
+  --       python = {
+  --         analysis = {
+  --           typeCheckingMode = "off",
+  --         },
+  --       },
+  --     }
+  --   }
+  --   goto continue
+  -- end
   --[[ if server == "pylsp" then ]]
   --[[   require("lspconfig").pylsp.setup { ]]
   --[[     settings = { ]]
@@ -105,27 +101,28 @@ for _, server in pairs(servers) do
   --[[ end ]]
 
   if server == "rust_analyzer" then
-    local keymap = vim.keymap.set
-    local key_opts = { silent = true }
 
-    keymap("n", "<leader>rh", "<cmd>RustSetInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>rhd", "<cmd>RustDisableInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>th", "<cmd>RustToggleInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>rr", "<cmd>RustRunnables<Cr>", key_opts)
-    keymap("n", "<leader>rem", "<cmd>RustExpandMacro<Cr>", key_opts)
-    keymap("n", "<leader>roc", "<cmd>RustOpenCargo<Cr>", key_opts)
-    keymap("n", "<leader>rpm", "<cmd>RustParentModule<Cr>", key_opts)
-    keymap("n", "<leader>rjl", "<cmd>RustJoinLines<Cr>", key_opts)
-    keymap("n", "<leader>rha", "<cmd>RustHoverActions<Cr>", key_opts)
-    keymap("n", "<leader>rhr", "<cmd>RustHoverRange<Cr>", key_opts)
-    keymap("n", "<leader>rmd", "<cmd>RustMoveItemDown<Cr>", key_opts)
-    keymap("n", "<leader>rmu", "<cmd>RustMoveItemUp<Cr>", key_opts)
-    keymap("n", "<leader>rsb", "<cmd>RustStartStandaloneServerForBuffer<Cr>", key_opts)
-    keymap("n", "<leader>rd", "<cmd>RustDebuggables<Cr>", key_opts)
-    keymap("n", "<leader>rv", "<cmd>RustViewCrateGraph<Cr>", key_opts)
-    keymap("n", "<leader>rw", "<cmd>RustReloadWorkspace<Cr>", key_opts)
-    keymap("n", "<leader>rss", "<cmd>RustSSR<Cr>", key_opts)
-    keymap("n", "<leader>rxd", "<cmd>RustOpenExternalDocs<Cr>", key_opts)
+    -- local keymap = vim.keymap.set
+    -- local key_opts = { silent = true }
+    --
+    -- keymap("n", "<leader>rh", "<cmd>RustSetInlayHints<Cr>", key_opts)
+    -- keymap("n", "<leader>rhd", "<cmd>RustDisableInlayHints<Cr>", key_opts)
+    -- keymap("n", "<leader>th", "<cmd>RustToggleInlayHints<Cr>", key_opts)
+    -- keymap("n", "<leader>rr", "<cmd>RustRunnables<Cr>", key_opts)
+    -- keymap("n", "<leader>rem", "<cmd>RustExpandMacro<Cr>", key_opts)
+    -- keymap("n", "<leader>roc", "<cmd>RustOpenCargo<Cr>", key_opts)
+    -- keymap("n", "<leader>rpm", "<cmd>RustParentModule<Cr>", key_opts)
+    -- keymap("n", "<leader>rjl", "<cmd>RustJoinLines<Cr>", key_opts)
+    -- keymap("n", "<leader>rha", "<cmd>RustHoverActions<Cr>", key_opts)
+    -- keymap("n", "<leader>rhr", "<cmd>RustHoverRange<Cr>", key_opts)
+    -- keymap("n", "<leader>rmd", "<cmd>RustMoveItemDown<Cr>", key_opts)
+    -- keymap("n", "<leader>rmu", "<cmd>RustMoveItemUp<Cr>", key_opts)
+    -- keymap("n", "<leader>rsb", "<cmd>RustStartStandaloneServerForBuffer<Cr>", key_opts)
+    -- keymap("n", "<leader>rd", "<cmd>RustDebuggables<Cr>", key_opts)
+    -- keymap("n", "<leader>rv", "<cmd>RustViewCrateGraph<Cr>", key_opts)
+    -- keymap("n", "<leader>rw", "<cmd>RustReloadWorkspace<Cr>", key_opts)
+    -- keymap("n", "<leader>rss", "<cmd>RustSSR<Cr>", key_opts)
+    -- keymap("n", "<leader>rxd", "<cmd>RustOpenExternalDocs<Cr>", key_opts)
 
     -- local ext_path = 'C:/Users/work/.vscode/extensions/lldb-172/'
     -- local codelldb_path = ext_path .. "adapter/codelldb"
@@ -137,10 +134,10 @@ for _, server in pairs(servers) do
       -- },
       tools = {
         on_initialized = function()
-          vim.cmd [[
-        autocmd BufEnter,CursorHold,InsertLeave,BufWritePost *.rs lua vim.lsp.codelens.refresh()
-        " autocmd BufEnter,CursorHold,InsertLeave,BufWritePost *.rs silent! lua vim.lsp.codelens.refresh()
-        ]]
+        --   vim.cmd [[
+        -- autocmd BufEnter,CursorHold,InsertLeave,BufWritePost *.rs lua vim.lsp.codelens.refresh()
+        -- " autocmd BufEnter,CursorHold,InsertLeave,BufWritePost *.rs silent! lua vim.lsp.codelens.refresh()
+        -- ]]
         end,
       },
       server = {
