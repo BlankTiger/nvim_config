b4822cc53b504757a9d527debd2d14875e0e9b52
Maciej Urban <maciej-urban@outlook.com>
2024-01-28 13:49:56 +0100

    fix(cmp): add back snippets, fix completion selection

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -114,17 +114,24 @@ return {
 			require("config.cmp")
 		end,
 		dependencies = {
-
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-nvim-lua",
 			--[[ "hrsh7th/cmp-copilot", ]]
-			-- "saadparwaiz1/cmp_luasnip",
+			"saadparwaiz1/cmp_luasnip",
 		}
 	},
+
 	-- "rafamadriz/friendly-snippets",
 	-- { "L3MON4D3/LuaSnip",               lazy = true },
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp"
+	},
 
 	{
 		"lewis6991/gitsigns.nvim",
